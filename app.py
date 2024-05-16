54e4b6dac4c16df94bca51bdffdbb6510ac90470
Aksay kanthan <aksaykanthan@gmail.com>
2024-05-16 19:42:35 +0530

    __

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ from user import routes
 def home():
     return render_template("home.html")
 
+# --------- PATIENT ROUTES ------------
+
 @app.route('/patient')
 @routes.login_required
 def patient():
@@ -37,7 +39,7 @@ def detailpatient():
     _id = request.args.get("_id")
     return render_template("patient/detailpatient.html",patient = Patient.get_patient(_id))
 
-# --------- hOSPITAL ROUTES ------------
+# --------- HOSPITAL ROUTES ------------
 
 @app.route("/hospital")
 def hospital():
